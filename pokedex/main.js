70fce2a765168e35834cb3dc3252a96e9705cd3b
Alejandra Bricio <alebricio@users.noreply.github.com>
2020-07-11 00:57:29 -0500

    Responsive Design

--- pokedex/main.js
+++ pokedex/main.js
@@ -64,38 +64,37 @@ function renderPokemons() {
 renderPokemons();
 
 function getOnePokemon() {
-    var gotPokemon = document.getElementById("pokemonInput").value 
+    var gotPokemon = document.getElementById("pokemonInput").value
     var getPokemon = gotPokemon - 1
-    console.log(getPokemon)
-    var div = document.createElement('div')
-    div.id = 'cardPicture'
-    div.className = 'card shadow-lg p-3 mb-5 bg-white rounded';
-    var img = document.createElement('img')
-    img.className = 'card-img-top'
-    img.style.width = '10rem'
-    img.setAttribute('src', pokemonPictures[getPokemon])
-    var cardBody = document.createElement('div')
-    cardBody.className = 'card-body'
-    var number = document.createElement('h1')
-    number.innerHTML = pokemonNumber[getPokemon]
-    number.className = 'card-text'
-    number.id = 'numberID'
-    var name = document.createElement('h3')
-    name.innerHTML = pokemonName[getPokemon]
-    name.className = 'card-text'
-    var type = document.createElement('h5')
-    type.innerHTML = 'Type: ' + pokemonType[getPokemon]
-    type.className = 'card-text'
-    var hp = document.createElement('h5')
-    hp.innerHTML = 'HP: ' + pokemonHp[getPokemon]
-    hp.className = 'card-text'
-    modalOnePokemon.appendChild(div)
-    div.appendChild(img)
-    div.appendChild(cardBody)
-    cardBody.appendChild(number)
-    cardBody.appendChild(name)
-    cardBody.appendChild(type)
-    cardBody.appendChild(hp)
+        var div = document.createElement('div')
+        div.id = 'cardPicture'
+        div.className = 'card shadow-lg p-3 mb-5 bg-white rounded';
+        var img = document.createElement('img')
+        img.className = 'card-img-top'
+        img.style.width = '10rem'
+        img.setAttribute('src', pokemonPictures[getPokemon])
+        var cardBody = document.createElement('div')
+        cardBody.className = 'card-body'
+        var number = document.createElement('h1')
+        number.innerHTML = pokemonNumber[getPokemon]
+        number.className = 'card-text'
+        number.id = 'numberID'
+        var name = document.createElement('h3')
+        name.innerHTML = pokemonName[getPokemon]
+        name.className = 'card-text'
+        var type = document.createElement('h5')
+        type.innerHTML = 'Type: ' + pokemonType[getPokemon]
+        type.className = 'card-text'
+        var hp = document.createElement('h5')
+        hp.innerHTML = 'HP: ' + pokemonHp[getPokemon]
+        hp.className = 'card-text'
+        modalOnePokemon.appendChild(div)
+        div.appendChild(img)
+        div.appendChild(cardBody)
+        cardBody.appendChild(number)
+        cardBody.appendChild(name)
+        cardBody.appendChild(type)
+        cardBody.appendChild(hp)
 }
 
 
